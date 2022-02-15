dd247123a200890d13432d711151c50f57fdfd80
Ahmadshouha <shouha96@outlook.com>
2022-02-15 23:01:31 +0200

    solved

--- code-challenges/challenges-06.js
+++ code-challenges/challenges-06.js
@@ -41,7 +41,7 @@ const sumNums = (arr)=>{
     sum = 0;
     for (let i = 0; i < arr.length; i++) {
 
-        if (typeof (arr[i]) == 'number') {
+        if (typeof (arr[i]) == 'num') {
 
             sum = sum + arr[i];
         }

d32222c923fff9b535475047a0f1375d4186a560
Ahmadshouha <shouha96@outlook.com>
2022-02-17 10:38:52 +0200

    add

--- code-challenges/challenges-18.js
+++ code-challenges/challenges-18.js
@@ -16,7 +16,9 @@
 
 const wordLength = (str) => {
     // write your code here
-}
+    let num = str.split(" ");
+    return num[Math.floor(num.length / 2)].length;
+  };
 // -------------------------------------------------------------------------------------------------------
 
 
@@ -41,7 +43,20 @@ const wordLength = (str) => {
 
 const wordLetters = (str1, str2) => {
     // write your code here
-}
+    let firststringlength = str1.length;
+    let secondstringlength = str2.length;
+    let string1Array = str1.split("").sort();
+    let string2Array = str2.split("").sort();
+    if (firststringlength != secondstringlength) {
+      return false;
+    }
+    for (let i = 0; i < firststringlength; i++) {
+      if (string1Array[i] != string2Array[i]) {
+        return false;
+      }
+    }
+    return true;
+  };
 // -------------------------------------------------------------------------------------------------------
 
 // -------------------------------------------------------------------------------------------------------

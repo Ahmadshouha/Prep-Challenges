cf179297a1f0aa2ce84be75ec3958d2aa9f65f9b
Ahmadshouha <shouha96@outlook.com>
2022-02-17 10:15:13 +0200

    add

---
+++ code-challenges/challenges-18.js
@@ -0,0 +1,87 @@
+'use strict';
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 01:
+// Required:
+//
+// Given a string find the length of the middle word in the sentence
+//
+// Ex:
+// Input: "You can't handle the truth!"
+// Output: 6
+//
+// Input: "You're gonna need a bigger boat"
+// Output: 1
+//
+
+const wordLength = (str) => {
+    // write your code here
+    const wordLength = (str) => {
+        let num = str.split(" ");
+        return num[Math.floor(num.length / 2)].length;
+}
+// -------------------------------------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 02:
+// Required:
+//
+// Write a function that takes two strings and checks if the two strings contain the same combination of letters
+//
+// Ex:
+// Input: "dad", "add"
+// Output: true
+//
+// Input: "dad", "aadd"
+// Output: false
+//
+// Input: "table", "label"
+// Output: false
+//
+// Input: "cat", "rat"
+// Output: false
+
+const wordLetters = (str1, str2) => {
+    let firstl = str1.length;
+    let secondl = str2.length;
+    let string1Arr = str1.split("").sort();
+    let string2Arr = str2.split("").sort();
+    if (firstl != secondl) {
+      return false;
+    }
+    for (let i = 0; i < firstl; i++) {
+      if (string1Arr[i] != string2Arr[i]) {
+        return false;
+      }
+    }
+    return true;
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+// -------------------------------------------------------------------------------------------------------
+// Challenge 03:
+// Optional:
+//
+// Write a function that takes an integer and a sorted array
+// The function returns the index of the integer in the array
+// If the integer was not there, the function returns where it could have been
+//
+// Ex:
+// Input: [1,3,5,6], 5
+// Output: 2
+//
+// Input: [1,3,5,6], 2
+// Output: 1
+//
+// Input: [1,3,5,6], 7
+// Output: 4
+//
+
+const targetIndex = (arr, int) => {
+    // write your code here
+}
+// -------------------------------------------------------------------------------------------------------
+
+module.exports = { wordLength, wordLetters, targetIndex };
